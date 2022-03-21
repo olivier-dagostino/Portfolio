82ef868658b26fb60ee9d7646187b7b65816d04e
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-21 18:23:52 +0100

    update background

--- presentation.php
+++ presentation.php
@@ -1,8 +1,8 @@
 <?php require 'header.php'; ?>
 
 <body>
-   
-        <section class="section2">
+
+    <section class="section2">
 
         <p id="introduction">Je suis un jeune épicurien
             de 30 ans, qui croque la vie à pleine dent,
@@ -24,106 +24,106 @@
         </p>
     </section>
 
-        
 
-        <h1>Mon Parcours</h1><br>
 
-        <div class="containerB-2">
+    <h1>Mon Parcours</h1><br>
 
-            <div class="exp-pro">
+    <div class="containerB-2">
 
-                <p>
-                <h2>Expériences Professionnelles</h2>
+        <div class="exp-pro">
 
-                <ul>
-                    <li>09/2021 - Aujourd'hui : Entrée à La Plateforme Marseille en "Coding School 1" (Marseille)</li>
-                    <br>
-                    <li>05/2021 - 09/2021 Responsable de Salle au RestoBar La Belle D'Ange (La Ciotat)</li>
-                    <br>
-                    <li>06/2020 - 09/2020 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
-                    <br>
-                    <li>04/2019 - 10/2019 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
-                    <br>
-                    <li>02/2018 - 10/2018 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
-                    <br>
-                    <li>06/2017 - 09/2017 Serveur - Restaurant Chez Yannick (La Ciotat)</li>
-                    <br>
-                    <li>11/2016 - 02/2017 Serveur - Restaurant Le Fer à Cheval (Gemenos)</li>
-                    <br>
-                    <li>05/2015 - 12/2015 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
-                    <br>
-                    <li>09/2013 - 04/2015 Barman, Serveur, Assistant Gérand - Restaurant Barbounya (Montréal Canada)</li>
-                    <br>
-                    <li>03/2013 - 08/2013 Barman, Serveur - Restaurant Confusion (Montréal Canada)</li>
-                    <br>
-                    <li>11/2012 - 02/2013 Équipier Polyvalent - Mc Donalds(La Ciotat)</li>
-                    <br>
-                    <li>11/2011 - 10/2012 Barman - Restaurant Chez Yannick (La Ciotat)</li>
-                    <br>
-                    <li>07/2011 - 07/2011 Employé Administratif - Allianz (Marseille)</li>
-                    <br>
-                    <li>02/2010 - 07/2011 Équipier Polyvalent - Mc Donalds</li>
-                    <br>
-                    <li>06/2010 - 09/2010 Barman - Restaurant Chez Yannick (La Ciotat)</li>
-                </ul>
-                </p>
+            <p>
+            <h2>Expériences Professionnelles</h2>
 
-            </div>
-
-            <div class="diplome">
-
-                <h2>Diplômes / Certifications</h2>
-
-                <ul>
-                    <li>2017 : Stage d'Anglais d'un mois (niveau B2) - New College Group (Liverpool Angleterre) </li>
-                    <br>
-                    <li>2017 : TOEIC En Cadidat Libre - Certification de niveau d'anglais (American Center Aix en Provence)<br>
-                        Score de 540 / 990</li>
-                    <br>
-                    <li>2016 : TOEIC Bridge - Certification de niveau d'anglais (SOGEFI LA CIOTAT)<br>
-                        Score de 154 / 180 (niveau B1)</li>
-                    <br>
-                    <li>2011 : BAC Professionnel SEN (Systèmes Électroniques et Numériques) - École Modèle d'Électronique (Marseille 11)<br></li>
-                    <br>
-                    <li>2010 : BEP SEID (Systèmes Électroniques Industriels et Domestique) - École Modèle d'Électronique (Marseille 11)<br></li>
-                </ul>
-
-            </div>
+            <ul>
+                <li>09/2021 - Aujourd'hui : Entrée à La Plateforme Marseille en "Coding School 1" (Marseille)</li>
+                <br>
+                <li>05/2021 - 09/2021 Responsable de Salle au RestoBar La Belle D'Ange (La Ciotat)</li>
+                <br>
+                <li>06/2020 - 09/2020 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
+                <br>
+                <li>04/2019 - 10/2019 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
+                <br>
+                <li>02/2018 - 10/2018 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
+                <br>
+                <li>06/2017 - 09/2017 Serveur - Restaurant Chez Yannick (La Ciotat)</li>
+                <br>
+                <li>11/2016 - 02/2017 Serveur - Restaurant Le Fer à Cheval (Gemenos)</li>
+                <br>
+                <li>05/2015 - 12/2015 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
+                <br>
+                <li>09/2013 - 04/2015 Barman, Serveur, Assistant Gérand - Restaurant Barbounya (Montréal Canada)</li>
+                <br>
+                <li>03/2013 - 08/2013 Barman, Serveur - Restaurant Confusion (Montréal Canada)</li>
+                <br>
+                <li>11/2012 - 02/2013 Équipier Polyvalent - Mc Donalds(La Ciotat)</li>
+                <br>
+                <li>11/2011 - 10/2012 Barman - Restaurant Chez Yannick (La Ciotat)</li>
+                <br>
+                <li>07/2011 - 07/2011 Employé Administratif - Allianz (Marseille)</li>
+                <br>
+                <li>02/2010 - 07/2011 Équipier Polyvalent - Mc Donalds</li>
+                <br>
+                <li>06/2010 - 09/2010 Barman - Restaurant Chez Yannick (La Ciotat)</li>
+            </ul>
+            </p>
 
         </div>
-        <center>
-            <h3>Compétences informatiques</h3>
-        </center>
-        <div class="competence">
-            <div class="competence1">
-                <div><img id="logo-html-css" src="img/logo_CSS_HTML.png" alt="logo-html-css"></div>
-                <div><img id="logo-github" src="img/logo_GitHub.png" alt="logo-github"></div>
-                <div><img id="logo-mysql" src="img/logo_mysql.png" alt="logo-mysql"></div>
-                <div><img id="logo-php" src="img/logo-php.png" alt="logo-php"></div>
+
+        <div class="diplome">
+
+            <h2>Diplômes / Certifications</h2>
+
+            <ul>
+                <li>2017 : Stage d'Anglais d'un mois (niveau B2) - New College Group (Liverpool Angleterre) </li>
+                <br>
+                <li>2017 : TOEIC En Cadidat Libre - Certification de niveau d'anglais (American Center Aix en Provence)<br>
+                    Score de 540 / 990</li>
+                <br>
+                <li>2016 : TOEIC Bridge - Certification de niveau d'anglais (SOGEFI LA CIOTAT)<br>
+                    Score de 154 / 180 (niveau B1)</li>
+                <br>
+                <li>2011 : BAC Professionnel SEN (Systèmes Électroniques et Numériques) - École Modèle d'Électronique (Marseille 11)<br></li>
+                <br>
+                <li>2010 : BEP SEID (Systèmes Électroniques Industriels et Domestique) - École Modèle d'Électronique (Marseille 11)<br></li>
+            </ul>
+
+        </div>
+
+    </div>
+    <center>
+        <h3>Compétences informatiques</h3>
+    </center>
+    <div class="competence">
+        <div class="competence1">
+            <div><img id="logo-html-css" src="img/logo_CSS_HTML.png" alt="logo-html-css"></div>
+            <div><img id="logo-github" src="img/logo_GitHub.png" alt="logo-github"></div>
+            <div><img id="logo-mysql" src="img/logo_mysql.png" alt="logo-mysql"></div>
+            <div><img id="logo-php" src="img/logo-php.png" alt="logo-php"></div>
+        </div>
+        <div class="competence2">
+            <div>
+                <img id="logo-pack-office" src="img/logo-packoffice-removebg-preview.png" alt="logo-pack-office">
             </div>
-            <div class="competence2">
-                <div>
-                    <img id="logo-pack-office" src="img/logo-packoffice-removebg-preview.png" alt="logo-pack-office">
-                </div>
-                <div>
-                    <img id="logo-g-suite" src="img/logo-g-suite.png" alt="logo-g-suite">
-                </div>
-                <div>
-                    <img id="logo-trello" src="img/trello.png" alt="">
-                </div>
+            <div>
+                <img id="logo-g-suite" src="img/logo-g-suite.png" alt="logo-g-suite">
             </div>
-            <div class="competence3">
-                <div>
-                    <img id="logo-procreate" src="img/Procreate-logo.png" alt="">
-                </div>
-                <div>
-                    <img id="logo-figma" src="img/figma.png" alt="">
-                </div>
-                <div>
-                    <img id="logo-wamp" src="img/logo-wamp.png" alt="">
-                </div>
+            <div>
+                <img id="logo-trello" src="img/trello.png" alt="">
             </div>
         </div>
+        <div class="competence3">
+            <div>
+                <img id="logo-procreate" src="img/Procreate-logo.png" alt="">
+            </div>
+            <div>
+                <img id="logo-figma" src="img/figma.png" alt="">
+            </div>
+            <div>
+                <img id="logo-wamp" src="img/logo-wamp.png" alt="">
+            </div>
+        </div>
+    </div>
 
     </div>
 
