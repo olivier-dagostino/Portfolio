04877bd214669b2d2f9d6bc42e15ad98fc795e47
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-22 18:43:09 +0100

    jour7 job1

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php require 'header.php'; ?>
 
-<main>
+<main class="main-accueil">
 
     <section class="section1">
 
-        <h1 id="titre-section1">Bienvenue</h1>
-        
-        <img id="avatar" src="./img/avataaars (1).png" alt="">
+        <div>
+            <h1 id="titre-section1">Bienvenue</h1>
+
+            <img id="avatar" src="./img/avataaars (1).png" alt="">
+        </div>
         <p class="txt-presentation">
             En reconversion professionnelle,<br>
             J’ai intégré La Plateforme Marseille en "Coding School 1" afin de devenir développeur Web et Web mobile.<br>
-            Je suis  la recherche d'une alternance à partir dès SEPTEMBRE 2022
+            Je suis la recherche d'une alternance à partir dès SEPTEMBRE 2022
         </p>
 
     </section>
--- presentation.php
+++ presentation.php
@@ -1,7 +1,6 @@
 <?php require 'header.php'; ?>
 
-<body>
-
+<main class="main-presentation">
     <section class="section2">
 
         <p id="introduction">Je suis un jeune épicurien
@@ -25,74 +24,8 @@
     </section>
 
 
-
-    <h1>Mon Parcours</h1><br>
-
-    <div class="containerB-2">
-
-        <div class="exp-pro">
-
-            <p>
-            <h2>Expériences Professionnelles</h2>
-
-            <ul>
-                <li>09/2021 - Aujourd'hui : Entrée à La Plateforme Marseille en "Coding School 1" (Marseille)</li>
-                <br>
-                <li>05/2021 - 09/2021 Responsable de Salle au RestoBar La Belle D'Ange (La Ciotat)</li>
-                <br>
-                <li>06/2020 - 09/2020 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
-                <br>
-                <li>04/2019 - 10/2019 Responsable de Salle RestoBar La Belle D'Ange (La Ciotat)</li>
-                <br>
-                <li>02/2018 - 10/2018 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
-                <br>
-                <li>06/2017 - 09/2017 Serveur - Restaurant Chez Yannick (La Ciotat)</li>
-                <br>
-                <li>11/2016 - 02/2017 Serveur - Restaurant Le Fer à Cheval (Gemenos)</li>
-                <br>
-                <li>05/2015 - 12/2015 Responsable de Salle - Restaurant Chez Yannick (La Ciotat)</li>
-                <br>
-                <li>09/2013 - 04/2015 Barman, Serveur, Assistant Gérand - Restaurant Barbounya (Montréal Canada)</li>
-                <br>
-                <li>03/2013 - 08/2013 Barman, Serveur - Restaurant Confusion (Montréal Canada)</li>
-                <br>
-                <li>11/2012 - 02/2013 Équipier Polyvalent - Mc Donalds(La Ciotat)</li>
-                <br>
-                <li>11/2011 - 10/2012 Barman - Restaurant Chez Yannick (La Ciotat)</li>
-                <br>
-                <li>07/2011 - 07/2011 Employé Administratif - Allianz (Marseille)</li>
-                <br>
-                <li>02/2010 - 07/2011 Équipier Polyvalent - Mc Donalds</li>
-                <br>
-                <li>06/2010 - 09/2010 Barman - Restaurant Chez Yannick (La Ciotat)</li>
-            </ul>
-            </p>
-
-        </div>
-
-        <div class="diplome">
-
-            <h2>Diplômes / Certifications</h2>
-
-            <ul>
-                <li>2017 : Stage d'Anglais d'un mois (niveau B2) - New College Group (Liverpool Angleterre) </li>
-                <br>
-                <li>2017 : TOEIC En Cadidat Libre - Certification de niveau d'anglais (American Center Aix en Provence)<br>
-                    Score de 540 / 990</li>
-                <br>
-                <li>2016 : TOEIC Bridge - Certification de niveau d'anglais (SOGEFI LA CIOTAT)<br>
-                    Score de 154 / 180 (niveau B1)</li>
-                <br>
-                <li>2011 : BAC Professionnel SEN (Systèmes Électroniques et Numériques) - École Modèle d'Électronique (Marseille 11)<br></li>
-                <br>
-                <li>2010 : BEP SEID (Systèmes Électroniques Industriels et Domestique) - École Modèle d'Électronique (Marseille 11)<br></li>
-            </ul>
-
-        </div>
-
-    </div>
     <center>
-        <h3>Compétences informatiques</h3>
+        <h1>SKILLS</h1>
     </center>
     <div class="competence">
         <div class="competence1">
@@ -126,7 +59,6 @@
     </div>
 
     </div>
-
-</body>
+</main>
 
 <?php require 'footer.php'; ?>
