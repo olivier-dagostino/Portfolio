ac69df09b65d8450fa6d3ac8cf28a68cdb8a1435
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-24 10:56:08 +0100

    update portfolio

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php require 'header.php'; ?>
 
-<main class="main-accueil">
+<main>
 
     <section class="section1">
 
         <div>
             <h1 id="titre-section1">Bienvenue</h1>
 
-            <img id="avatar" src="./img/avataaars (1).png" alt="">
+
         </div>
         <p class="txt-presentation">
             En reconversion professionnelle,<br>
@@ -15,6 +15,98 @@
             Je suis la recherche d'une alternance à partir dès SEPTEMBRE 2022
         </p>
 
+    </section>
+    <section class="section2">
+        <div id="intro-container">
+            <p id="introduction">Je suis un jeune épicurien
+                de 30 ans, qui croque la vie à pleine dent,
+                aux tendances GlobeTrotter...<br>
+                J'ai un parcours assez atypique.
+                BAC PRO SEN (Systèmes électroniques
+                et Numériques) passé en 2011.<br>
+                Je travaillais dans la restauration
+                durant mes études, et je suis ensuite
+                partie à l'aventure durant presque
+                3 ans à Montréal à l'age de 20 ans.<br>
+                11 ans dans la restauration dans deux
+                pays différents. <br>
+                Cela fait déjà 4 ans que j'essai de
+                quitter la restauration, et depuis fin
+                septembre 2021, je suis entré à La
+                Plateforme Marseille afin de devenir
+                un développeur Web et Web Mobile.
+            </p>
+            <div class="pdf">
+                <h3>Télécharger mon CV</h3>
+
+                <a href="cv-olivierD.pdf" download><img id="logo-pdf" src="./img/Sans titre (250 × 250 px).png" alt=""></a>
+            </div>
+        </div>
+
+        <center>
+            <h1>SKILLS</h1>
+        </center>
+        <div class="competence">
+            <div class="competence1">
+                <div><img id="logo-html-css" src="img/logo_CSS_HTML.png" alt="logo-html-css"></div>
+                <div><img id="logo-github" src="img/logo_GitHub.png" alt="logo-github"></div>
+                <div><img id="logo-mysql" src="img/logo_mysql.png" alt="logo-mysql"></div>
+                <div><img id="logo-php" src="img/logo-php.png" alt="logo-php"></div>
+            </div>
+            <div class="competence2">
+                <div>
+                    <img id="logo-pack-office" src="img/logo-packoffice-removebg-preview.png" alt="logo-pack-office">
+                </div>
+                <div>
+                    <img id="logo-g-suite" src="img/logo-g-suite.png" alt="logo-g-suite">
+                </div>
+                <div>
+                    <img id="logo-trello" src="img/trello.png" alt="">
+                </div>
+            </div>
+            <div class="competence3">
+                <div>
+                    <img id="logo-procreate" src="img/Procreate-logo.png" alt="">
+                </div>
+                <div>
+                    <img id="logo-figma" src="img/figma.png" alt="">
+                </div>
+                <div>
+                    <img id="logo-wamp" src="img/logo-wamp.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </section>
+    <section class="section3">
+    <h1 id="title-project">Mes projets</h1>
+        <div class="container3">
+
+            <div class="project">
+                <h4>Module de Connexion</h2> <br>
+                    <a href="module-connexion/index.php"><img id="myImg" src="img/module-connexion.png" alt="aperçu"></a>
+            </div>
+
+            <div class="project">
+                <h4>Livre d'Or</h4> <br>
+                <a href="livreor2.0/index.php"><img id="myImg" src="img/ livre-or.png" alt="aperçu"></a>
+            </div>
+
+            <div class="project">
+                <h4>Site de Voyage</h4> <br>
+                <a href="voyage/index.html"><img id="myImg" src="img/voyage.png" alt="aperçu"></a>
+            </div>
+            <div class="project">
+                <h4>Réservation de Salles</h4> <br>
+                <a href="reservation-salles/index.php"><img id="myImg" src="img/Capture d’écran 2022-02-07 à 19.18.16.png" alt="aperçu"></a>
+            </div>
+            <div class="project">
+                <h4>Blog</h4> <br>
+                <a href="blog/index.php"><img id="myImg" src="img/blog.png" alt="aperçu"></a>
+            </div>
+
+        </div>
     </section>
 
 
--- presentation.php
+++ presentation.php
@@ -1,64 +1,7 @@
 <?php require 'header.php'; ?>
 
 <main class="main-presentation">
-    <section class="section2">
-
-        <p id="introduction">Je suis un jeune épicurien
-            de 30 ans, qui croque la vie à pleine dent,
-            aux tendances GlobeTrotter...<br>
-            J'ai un parcours assez atypique.
-            BAC PRO SEN (Systèmes électroniques
-            et Numériques) passé en 2011.<br>
-            Je travaillais dans la restauration
-            durant mes études, et je suis ensuite
-            partie à l'aventure durant presque
-            3 ans à Montréal à l'age de 20 ans.<br>
-            11 ans dans la restauration dans deux
-            pays différents. <br>
-            Cela fait déjà 4 ans que j'essai de
-            quitter la restauration, et depuis fin
-            septembre 2021, je suis entré à La
-            Plateforme Marseille afin de devenir
-            un développeur Web et Web Mobile.
-        </p>
-    </section>
-
-
-    <center>
-        <h1>SKILLS</h1>
-    </center>
-    <div class="competence">
-        <div class="competence1">
-            <div><img id="logo-html-css" src="img/logo_CSS_HTML.png" alt="logo-html-css"></div>
-            <div><img id="logo-github" src="img/logo_GitHub.png" alt="logo-github"></div>
-            <div><img id="logo-mysql" src="img/logo_mysql.png" alt="logo-mysql"></div>
-            <div><img id="logo-php" src="img/logo-php.png" alt="logo-php"></div>
-        </div>
-        <div class="competence2">
-            <div>
-                <img id="logo-pack-office" src="img/logo-packoffice-removebg-preview.png" alt="logo-pack-office">
-            </div>
-            <div>
-                <img id="logo-g-suite" src="img/logo-g-suite.png" alt="logo-g-suite">
-            </div>
-            <div>
-                <img id="logo-trello" src="img/trello.png" alt="">
-            </div>
-        </div>
-        <div class="competence3">
-            <div>
-                <img id="logo-procreate" src="img/Procreate-logo.png" alt="">
-            </div>
-            <div>
-                <img id="logo-figma" src="img/figma.png" alt="">
-            </div>
-            <div>
-                <img id="logo-wamp" src="img/logo-wamp.png" alt="">
-            </div>
-        </div>
-    </div>
-
-    </div>
+    
 </main>
 
 <?php require 'footer.php'; ?>
